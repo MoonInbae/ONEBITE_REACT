5fb5f494a4538866f0b69df4bcd7461b7aafd8a9
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2025-07-22 00:28:30 +0900

    Chapter05 - 자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,39 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+let inf = Infinity;
+let mIng = -Infinity;
+
+let nan = NaN;
+
+// console.log(1 * "hello");
+
+// 2. String Type
+let myName = "문인배";
+let myLocation = "계룡시";
+let introduce = myName + myLocation;
+
+// console.log(introduce);
+
+// 템플릿 리터럴 문법
+let introduceText = `${myName}는 ${myLocation}에 거주합니다.`;
+// console.log(introduceText);
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
