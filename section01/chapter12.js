c835aff4e67d62a87a233195e1cb54636c279008
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2025-07-26 00:00:22 +0900

    Chapter12 - 함수 표현식과 화살표 함수

---
+++ section01/chapter12.js
@@ -0,0 +1,25 @@
+// 1. 함수 표현식식
+function funcA() {
+    console.log('funcA');
+}
+
+let varA = funcA;
+varA();
+
+let varB = function funcB() {
+    console.log('funcB');
+};
+
+varB();
+
+//2. 화살표 함수
+// let varC = () => {
+//     return 1;
+// }
+
+let varC = (value) => {
+    console.log(value);
+    return value + 1;
+};
+
+console.log(varC());
