976da7bba5b13c8a507a27fa282a2ecfe5ff23f3
Inbae-Moon <81071736+MoonInbae@users.noreply.github.com>
2025-07-30 00:22:56 +0900

    Chapter13 - 콜백함수

--- section01/chapter13.js
+++ section01/chapter13.js
@@ -19,3 +19,56 @@ function main(value) {
 main(() => {
     // console.log('i am sub');
 });
+
+// 2. 콜백함수의 활용
+// function repeat(count) {
+//     for (let idx = 1; idx <= count; idx++) {
+//         console.log(idx);
+//     }
+// }
+
+// function repeatDouble(count) {
+//     for (let idx = 1; idx <= count; idx++) {
+//         console.log(idx * 2);
+//     }
+// }
+
+// function repeatTriple(count) {
+//     for (let idx = 1; idx <= count; idx++) {
+//         console.log(idx * 3);
+//     }
+// }
+
+// repeat(5);
+// repeatDouble(5);
+// repeatTriple(5);
+
+function repeat(count, callback) {
+    for (let idx = 1; idx <= count; idx++) {
+        callback(idx);
+    }
+}
+
+// repeat(5, function (idx) {
+//     console.log(idx);
+// });
+
+// repeat(5, function (idx) {
+//     console.log(idx * 2);
+// });
+
+// repeat(5, function (idx) {
+//     console.log(idx * 3);
+// });
+
+repeat(5, (idx) => {
+    console.log(idx);
+});
+
+repeat(5, (idx) => {
+    console.log(idx * 2);
+});
+
+repeat(5, (idx) => {
+    console.log(idx * 3);
+});
